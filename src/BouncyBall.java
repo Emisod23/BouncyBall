2271096639ebaccb8c6b68b881c48c7c33a53ae9
Hera-Heeto <Emil.soderlund@elev.ga.ntig.se>
2021-08-26 14:06:24 +0200

    Bouncy ball

--- src/BouncyBall.java
+++ src/BouncyBall.java
@@ -1,3 +1,16 @@
 public class BouncyBall {
-    
+    public static void main(String[] args){
+        double Ballx;
+        double Bally = 100;
+        double Ballpos;
+        double Ballspeed = 0;
+        double Ballacc = 1;
+        double Time = 0;
+
+        for(int i = 0; i >= 120; ++i);{
+            --Ballspeed;
+            Ballpos = Bally - Ballspeed;
+            System.out.println(Ballpos);
+        }
+    }
 }
